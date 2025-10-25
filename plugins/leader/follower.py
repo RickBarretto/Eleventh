c53761e1010c3961cd1cd3c6d0882f3c1c0523a4
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-10-24 21:58:53 -0300

    [Plugins\Leader] Implement a Follower for Leader-Follower pattern

---
+++ plugins/leader/follower.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from sys import prefix
+from typing import Protocol
+
+import attrs
+
+from httpx import AsyncClient
+
+
+__all__ = ["Follower"]
+
+
+@attrs.define
+class Follower:
+    id: str
+    url: str
+    follows: Leader
+    _prefix: str = "/leader"
+
+    @property
+    def prefix(self) -> str:
+        return self._prefix.strip("/")
+
+    @property
+    def client(self) -> AsyncClient:
+        return AsyncClient(base_url=self.follows.url + self.prefix)
+
+    async def has_leader(self) -> bool:
+        try:
+            response = await self.client.get("/")
+            return response.is_success
+        except Exception:
+            return False
+        
+
+class Leader(Protocol):
+    id: str
+    url: str
