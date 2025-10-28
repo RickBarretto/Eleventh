ca02cf45b023f0162f8dd2d19a8e0fc9b353bf7d
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-10-27 22:35:46 -0300

    Remove v2 files
    Signed-off-by: RickBarretto <78623871+RickBarretto@users.noreply.github.com>

--- .project/goals/pubsub/config/config.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-# Configure logger
-config :logger, :console,
-  level: :info,
-  format: "$date $time [$level] $metadata$message\n"
-
-# Import environment specific config
-import_config "#{config_env()}.exs"
